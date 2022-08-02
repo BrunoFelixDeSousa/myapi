2a6d209b63d993c6650c99e3c62e5731c12efd4f
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-01 22:13:04 -0300

    classe usuario criado

---
+++ src/main/java/com/bruno/myapi/domain/Usuario.java
@@ -0,0 +1,82 @@
+package com.bruno.myapi.domain;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    @Length(min = 3, max = 100, message = "O nome deve ter entre 3 a 100 caracteres")
+    private String nome;
+
+    @NotEmpty(message = "campo LOGIN é mandatório")
+    @Length(min = 3, max = 100, message = "O login deve ter entre 3 a 100 caracteres")
+    private String login;
+
+    @NotEmpty(message = "campo SENHA é mandatório")
+    @Length(min = 3, max = 100, message = "A senha deve ter entre 3 a 100 caracteres")
+    private String senha;
+
+    public Usuario() {
+        super();
+    }
+
+    public Usuario(Integer id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
