b3fc944262445391ac65212847d262dc3a380ec9
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-02 21:46:23 -0300

    Tratamento de exceções

--- src/main/java/com/bruno/myapi/services/UsuarioService.java
+++ src/main/java/com/bruno/myapi/services/UsuarioService.java
@@ -2,6 +2,7 @@ package com.bruno.myapi.services;
 
 import com.bruno.myapi.domain.Usuario;
 import com.bruno.myapi.repositories.UsuarioRepsoitory;
+import com.bruno.myapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class UsuarioService {
 
     public Usuario findById(Integer id) {
         Optional<Usuario> obj = repsoitory.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Usuario.class.getName()));
     }
 
 }
