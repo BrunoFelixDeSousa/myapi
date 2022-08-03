b371040c0a8b83546127b477fef30dfed8868f1f
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-02 22:03:19 -0300

    Listando todos os usuarios

--- src/main/java/com/bruno/myapi/resource/UsuarioResouce.java
+++ src/main/java/com/bruno/myapi/resource/UsuarioResouce.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResouce {
@@ -20,5 +22,11 @@ public class UsuarioResouce {
     public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
         Usuario obj = this.service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll() {
+        List<Usuario> list =  service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/bruno/myapi/services/UsuarioService.java
+++ src/main/java/com/bruno/myapi/services/UsuarioService.java
@@ -6,6 +6,7 @@ import com.bruno.myapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,10 @@ public class UsuarioService {
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Usuario.class.getName()));
     }
+
+    public List<Usuario> findAll() {
+        return repsoitory.findAll();
+    }
+
 
 }
